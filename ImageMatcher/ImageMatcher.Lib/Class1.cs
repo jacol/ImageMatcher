0c76af9e2f0cc68d614e2fa4756e5ec3a205223e
Jacek <jnikitiuk@otrzy.pl>
2019-11-01 10:48:11 +0100

    Initial commit

---
+++ ImageMatcher/ImageMatcher.Lib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ImageMatcher.Lib
+{
+    public class Class1
+    {
+    }
+}
---
+++ ImageMatcher/ImageMatcher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImageMatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
